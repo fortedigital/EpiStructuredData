bda6d399e6ed8f1fa4d273b2ea2da58cd0ebf256
Łukasz Potaczek <lukasz.potaczek@fortedigital.no>
2020-12-14 09:34:54 +0100

    Add missing StructuredDataGenerator

---
+++ src/EpiStructuredData/StructuredDataGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace EpiStructuredData
+{
+    public abstract class StructuredDataGenerator<T, TStructuredData> : IStructuredDataGenerator
+    {
+        public object GetStructuredData(object input)
+        {
+            if (!(input is T data)) return null;
+            return GetStructuredData(data);
+        }
+
+        protected abstract TStructuredData GetStructuredData(T input);
+    }
+}
